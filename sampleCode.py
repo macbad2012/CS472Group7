87c3b4e286660486509e4f3bb75dd47a1c9eec19
jssomerville <jssomerville2@gmail.com>
2017-01-18 15:31:37 -0500

    added more Watson AlchemyLanguage sample functions

--- sampleCode.py
+++ sampleCode.py
@@ -14,13 +14,51 @@ def title(link):
     return info["title"]
 
 def keywords(link):
-    response = json.dumps(alchemy_language.keywords(url=link), indent=2)
+    response = json.dumps(alchemy_language.keywords(url=link),indent=2)
     info = json.loads(response)
     return info
 
-"""print(keywords(link)["keywords"])"""
+def authors(link):
+    response = json.dumps(alchemy_language.authors(url=link),indent=2)
+    info= json.loads(response)
+    return info["authors"]
 
+def concepts(link):
+    response = json.dumps(alchemy_language.concepts(url=link),indent=2)
+    info = json.loads(response)
+    return info
+
+def date(link):
+    response = json.dumps(alchemy_language.date(url=link),indent=2)
+    info = json.loads(response)
+    return info
+
+def emotion(link):
+    response = json.dumps(alchemy_language.emotion(url=link),indent=2)
+    info = json.loads(response)
+    return info
+
+def relations(link):
+    response = json.dumps(alchemy_language.relations(url=link, max_items=2), indent=2)
+    info = json.loads(response)
+    return info
+
+"""
 for i in keywords(link)["keywords"]:
-    if (float(i['relevance']) > 0.75) :
+    if (float(i['relevance']) > 0.5) :
         print i    
-        
+"""
+
+"""printing concepts
+for i in concepts(link)["concepts"]:
+    if (float(i['relevance']) > 0.75) :
+        print i        
+"""
+
+"""print date(link)"""
+
+info = relations(link)
+for i in info["relations"]:
+    print 'sentence:  ' + i['sentence']
+    for j in i['object'] :
+        print j
