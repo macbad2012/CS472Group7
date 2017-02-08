f594d34a73a5a015cd1dde2009dc928900b00457
jssomerville <jssomerville2@gmail.com>
2017-02-08 14:34:19 -0500

    Moved the parsing to 'alchemyLangParse.py' and finished parsing the relations

--- alchemyLangParse.py
+++ alchemyLangParse.py
@@ -3,8 +3,6 @@
 from sys import argv
 from watson_developer_cloud import AlchemyLanguageV1
 import json
-import os
-import pprint
 
 script, link = argv
 
@@ -13,13 +11,23 @@ alchemy_language = AlchemyLanguageV1(api_key=ALCHEMY_LANGUAGE_KEY)
 
 def relations(link):
     #response is set to json.dumps (string data type) of the relation found in the article
-    response = json.dumps(alchemy_language.relations(url=link, max_items=1), indent=2)
+    response = json.dumps(alchemy_language.relations(url=link, max_items=2), indent=2)
     
     #obj is set to json.loads (object format) of the string that response was set to
     obj = json.loads(response)
 
     return obj
 
+"""
+    This block of code traverses the dictionary that the relations call returns from Watson.
+    This will give a subject, action, and object of an individual claim as well as the claim itself.
+    @param subject: the noun of the claim that is performing the action
+    @param object: the noun of the claim that is being acted upon
+    @param action: the verb of the claim that the subject is using on the object
+    
+    TODO: Need to find a way to get more than one claim from a single article
+    TODO: Potentially less arbitrary commenting
+"""
 #sets info to the relation
 info = relations(link)
 
@@ -38,6 +46,13 @@ for i in info["relations"]:
     for j in i.items():
         #j is the base list of relations (location subject sentence action object)
         for k in j:
+            # k is each item in the relations
+            # location is a dict
+            # subject is a dict
+            # sentence is a string
+            # action is a dict
+            # object is a dict
+
             if (isSentence == 1):
                 sentence = k
                 isSentence = 0
@@ -47,13 +62,7 @@ for i in info["relations"]:
                 isObjPre = 1
             elif (k == "sentence"):
                 isSentence = 1
-            
-            # k is each item in the relations
-            # location is a dict
-            # subject is a dict
-            # sentence is a string
-            # action is a dict
-            # object is a dict
+
             if (isinstance(k, dict)):
                 for l in k.items():
                     # l is the contents of k (excluding the sentence itself which is present in its primitive form in k)
@@ -61,6 +70,7 @@ for i in info["relations"]:
                     # the second is the subject
                     # the third and fourth are the lemmatized version of the verb and how its present in the article
                     # the fifth is the object the verb acts upon
+
                     for m in l:
                         # m is the primitive instances of each object extracted in l
                         if (isSubject == 1):
