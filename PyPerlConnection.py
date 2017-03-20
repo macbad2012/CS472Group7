f4a9b91ad94c84e5428e36947535d0984726e2ff
tjreardon <tjreardon@mix.wvu.edu>
2017-03-20 17:28:29 -0400

    remove old files

--- PyPerlConnection.py
+++
@@ -1,9 +0,0 @@
-#When finished, this code should be copied into the main program, and modified as necessary
-import subprocess
-
-url = "facebook.com" #can be full url
-pathToPerlScript ='C:/Users/Stephen/Downloads/CS472Group7-master/CS472Group7-master' #CHANGE THIS!
-pipe = subprocess.Popen(["perl", "wot.pl",url, pathToPerlScript], stdout=subprocess.PIPE)
-perl_stdout = pipe.communicate(input=pathToPerlScript)
-responce = perl_stdout[0].decode().replace('\r','').split('\n')
-print(responce)
